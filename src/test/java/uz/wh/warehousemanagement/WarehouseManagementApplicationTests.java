a3840b71da8cbd33bc4572347897b88db1218fb6
Alisher Kasimov <alisherkasimov1994@gmail.com>
2020-02-17 15:19:21 +0500

    Initial commit; Application created;

---
+++ src/test/java/uz/wh/warehousemanagement/WarehouseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.wh.warehousemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
