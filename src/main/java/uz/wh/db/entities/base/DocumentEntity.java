03b6d36e52de3d47413000fb66c25e0efc7ddb0a
Uchqun Bahodirovich <uchqunbek1729@gmail.com>
2020-02-18 16:56:57 +0500

    Added abstract DocumentEntity class;

---
+++ src/main/java/uz/wh/db/entities/base/DocumentEntity.java
@@ -0,0 +1,23 @@
+package uz.wh.db.entities.base;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class DocumentEntity extends BaseEntity {
+
+    @Column(name = "reference_number", unique = true)
+    private String reference;
+
+    @Column(name = "order_date")
+    private LocalDateTime orderDate;
+
+    @Column(name = "user_id")
+    private int userId;
+
+}
